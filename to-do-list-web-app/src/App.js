c2f57a82a07fd4056d9f4d48fd54b423856fb37e
dino1210 <147677268+dino1210@users.noreply.github.com>
2024-01-17 19:05:15 +0800

    Code for a new web app

--- to-do-list-web-app/src/App.js
+++ to-do-list-web-app/src/App.js
@@ -1,34 +1,46 @@
-import React from'react';
-import { Link } from "react-router-dom";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Tasks from './components/Tasks';
-import Contacts from './components/Contacts';
-import About from './components/About';
-import './App.css'; 
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import Cart from "./components/Cart";
+import Orders from "./components/Orders";
+import Account from "./components/Account";
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <nav className="app-nav">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/tasks">Tasks</Link></li>
-          <li><Link to="/contacts">Contacts</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
-      <main className="app-main">
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/menu">Menu</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+            <li>
+              <Link to="/orders">Orders</Link>
+            </li>
+            <li>
+              <Link to="/account">Account</Link>
+            </li>
+          </ul>
+        </nav>
+
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
-      </main>
-    </BrowserRouter>
+      </div>
+    </Router>
   );
-}
-      <footer className="footer"></footer>
+};
 
 export default App;
