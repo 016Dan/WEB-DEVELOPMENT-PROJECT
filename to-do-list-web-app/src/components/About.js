d34e0b4d695edf3133367b6568ea38c94f1c917d
JesDC <jm.decastro38@gmail.com>
2024-01-17 23:44:34 +0800

    Create About.js

---
+++ to-do-list-web-app/src/components/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+        <p className=" text-3">
+          <br />
+          Craving delicious snacks without leaving campus? Dive into
+          CampusCraveHub, your one-stop platform for exploring, ordering, and
+          enjoying a vibrant marketplace of student-made culinary creations!
+          <br />
+          Here's where TUP students become micro-entrepreneurs, unleashing their
+          inner chefs and sharing their passion for food. Discover homemade
+          delights, baked goods with love, and quick bites that fuel your day.
+          <br />
+          CampusCraveHub is more than just a food app. It's a platform for
+          convenience, connection, and community. Support your peers, explore
+          diverse flavors, and fuel your studies - all within the tap of your
+          finger.
+          <br />
+          Welcome to CampusCraveHub, the TUP E-canteen where your cravings meet
+          student dreams. We invite you to savor the taste of ambition, support
+          your fellow students, and experience the future of campus dining.
+          <br />
+          #CampusCraveHub #TUPEats #Tupians #CommunityEats
+        </p>
+    </div>
+  );
+};
+
+export default About;
