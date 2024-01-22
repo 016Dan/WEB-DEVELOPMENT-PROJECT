bb3ef218b3ad0ed67d75665cfe7d844a49915ab5
016Dan <131225375+016Dan@users.noreply.github.com>
2024-01-22 21:45:48 +0800

    remover the marker in the app-nav section
    remover the marker in the app-nav section

--- to-do-list-web-app/src/App.js
+++ to-do-list-web-app/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
         <nav>
           {loggedIn && (
             <li className="app-nav">
-              <li>Welcome, {username}! </li>
+              <li>WELCOME {username}! </li>
               <li>
                 <Link to="/home">Home</Link>
               </li>
